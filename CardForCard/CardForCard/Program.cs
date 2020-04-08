b38e0c906558b00c45720d9686aa5c7c4d546cc1
Dani <danielpaskalev@yahoo.com>
2020-04-08 20:40:33 +0300

    create solution

---
+++ CardForCard/CardForCard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardForCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
